19452cd99d8d958c61d3422c574f0a6d641533ba
郭超 <guochao@puhuifinance.com>
2017-02-12 17:12:37 +0800

    添加代理首页和汇总统计界面

---
+++ src/js/idea.js
@@ -0,0 +1,11 @@
+var Idea = {
+    init: function(){
+        this.setUIEvent();
+    },
+    setUIEvent: function(){
+        // table 的展示
+        $('.idea-table-label-js').on('click', function(){
+            alert('??/')
+        })
+    }
+}
